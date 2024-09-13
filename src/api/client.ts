29bdc0e5356bccf9f66ef55026b767fe24100046
eliezerBrasilian <eliezerassuncaocustodio@hotmail.com>
2024-09-13 14:39:30 -0300

    update url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1/meu-discord",
+  baseURL: "http://157.230.143.9:8080/api/v1/meu-discord",
 });
